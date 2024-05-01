c1df41595ca371dc8abfdb808a0a6d1f715aeba9
VictoryChibueze <alhajivicci12@gmail.com>
2024-05-01 15:08:58 +0100

    week01

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,5 @@
-let copyrightYearEl = document.g
+let copyrightYearEl = document.getElementById("lastModified");
+let yearEl = document.getElementById("year");
+
+copyrightYearEl.textContent = new Date(document.lastModified);
+yearEl.textContent = new Date().getFullYear();
