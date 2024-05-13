11e0a3e5cb3eb10ac6570bae59f06e9ea6a6efce
VictoryChibueze <alhajivicci12@gmail.com>
2024-05-13 11:25:08 +0100

    Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,11 @@ let yearEl = document.getElementById("year");
 
 copyrightYearEl.textContent = new Date(document.lastModified);
 yearEl.textContent = new Date().getFullYear();
+
+const navElement = document.querySelector("nav");
+const hamButton = document.querySelector("#ham-button");
+
+hamButton.addEventListener("click", () => {
+  navElement.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
