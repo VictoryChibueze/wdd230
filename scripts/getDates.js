c906741942eaf64f73243c40a36bc4db60b1b23b
VictoryChibueze <alhajivicci12@gmail.com>
2024-05-23 07:58:44 +0100

    week 03 page enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,15 @@ let yearEl = document.getElementById("year");
 
 copyrightYearEl.textContent = new Date(document.lastModified);
 yearEl.textContent = new Date().getFullYear();
+
+const visits = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+  visits.textContent = numVisits;
+} else {
+  visits.textContent = `This is your first visit. Welcome!`;
+}
+
+numVisits++;
