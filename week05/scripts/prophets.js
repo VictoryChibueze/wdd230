7810f886f6b7af9f9ff6a59718d8cdea6315a176
VictoryChibueze <alhajivicci12@gmail.com>
2024-06-05 07:50:14 +0100

    week05

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -9,9 +9,29 @@ async function getProphetData() {
   const data = await response.json();
   //   console.table(data.prophets);
 
-  displayProphets();
+  displayProphets(data.prophets);
 }
 
-const displayProphets = (prophets) => {};
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    const card = document.createElement("section");
+    const section = document.createElement("section");
+    const fullName = document.createElement("h2");
+    const portrait = document.createElement("img");
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("alt", "A Potrait of a latter day prophet");
+    portrait.setAttribute("width", 340);
+    portrait.setAttribute("height", 440);
+    portrait.setAttribute("loading", "lazy");
+
+    card.append(fullName);
+    card.append(portrait);
+
+    cards.append(card);
+  });
+};
 
 getProphetData();
