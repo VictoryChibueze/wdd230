cc37ea0729fa42bf363e8773108f95799bc33461
VictoryChibueze <alhajivicci12@gmail.com>
2024-06-07 03:28:44 +0100

    finals

--- scripts/links.js
+++ scripts/links.js
@@ -4,10 +4,20 @@ const baseURL = "https://victorychibueze.github.io/wdd230/";
 
 const linkURL = "https://victorychibueze.github.io/wdd230/data/links.json";
 
+const learningActivitiesEl = document.querySelector(".learning-activities");
+
 async function getLinks() {
   const response = await fetch(linkURL);
   const data = await response.json();
 
-  console.table(data);
+  console.table(data.weeks);
+
+  displayLinks(data.weeks);
 }
 getLinks();
+
+const displayLinks = (weeks) => {
+  weeks.forEach((week) => {
+    const li = document.createElement("li");
+  });
+};
